27929f59fd94c5c7b20ed4a9e9aa47faf74c6c5d
Lihaozhe <55686617+Lafilosofia@users.noreply.github.com>
2020-11-29 10:02:11 +0800

    Add files via upload

---
+++ http/EmptyRequestException.java
@@ -0,0 +1,31 @@
+package src.com.ykt.webServer.http;
+
+public class EmptyRequestException extends RuntimeException {
+
+	public EmptyRequestException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmptyRequestException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmptyRequestException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmptyRequestException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmptyRequestException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
